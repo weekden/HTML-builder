dcbf6ba4249ba86ddf9cfd2cadb74a727f52e3cd
DenisNedelko <dnsndlk@gmail.com>
2025-01-19 18:43:27 +0300

    refactor: change for fs/promises 2025-01-19 18:43:26

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,28 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+const promises = require('fs/promises');
 const folderPath = path.join(__dirname, './secret-folder');
-fs.readdir(folderPath, { withFileTypes: true }, (err, file) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  file.forEach((item) => {
-    if (item.isFile()) {
-      let resultInfo = '';
-      const dotIndex = item.name.lastIndexOf('.');
-      const fileName = item.name.slice(0, dotIndex);
-      const extFileName = item.name.slice(dotIndex + 1);
-      const filePath = path.join(folderPath, item.name);
 
-      fs.stat(filePath, (error, info) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        const fileSize = info.size;
-        resultInfo = `${fileName} - ${extFileName} - ${fileSize} kb`;
-        console.log(resultInfo);
-      });
-    }
-  });
-});
+promises.readdir(folderPath, { withFileTypes: true }).then(
+  (file) => {
+    file.forEach((item) => {
+      if (item.isFile()) {
+        let resultInfo = '';
+        const dotIndex = item.name.lastIndexOf('.');
+        const fileName = item.name.slice(0, dotIndex);
+        const extFileName = item.name.slice(dotIndex + 1);
+        const filePath = path.join(folderPath, item.name);
+
+        fs.stat(filePath, (error, info) => {
+          if (error) {
+            console.error(error);
+            return;
+          }
+          const fileSize = info.size;
+          resultInfo = `${fileName} - ${extFileName} - ${fileSize} kb`;
+          console.log(resultInfo);
+        });
+      }
+    });
+  },
+  (err) => {
+    console.error(err);
+  },
+);
