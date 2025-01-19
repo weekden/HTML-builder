f0f122b1dcb1f235f3b7016199747e698f4d5d41
DenisNedelko <dnsndlk@gmail.com>
2025-01-19 18:57:52 +0300

    refactor: change for fs/promises 2025-01-19 18:57:52

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const promises = require('fs/promises');
 const copyDirectoryName = 'files-copy';
 const sourceFolderPath = path.join(__dirname, './files');
 const distanationFolderPath = path.join(__dirname, `./${copyDirectoryName}`);
+
 (function copyFile() {
   fs.mkdir(distanationFolderPath, { recursive: true }, (err) => {
     if (err) console.error(err);
     console.log(`Directory <${copyDirectoryName}> was created`);
   });
-  fs.readdir(sourceFolderPath, { withFileTypes: true }, (err, files) => {
-    if (err) console.error(err);
-    files.forEach((file) => {
-      if (file.isFile()) {
-        const sourceFilePath = path.join(sourceFolderPath, file.name);
-        const distinationFilePath = path.join(distanationFolderPath, file.name);
-        fs.copyFile(sourceFilePath, distinationFilePath, (err) => {
-          if (err) console.error(err);
-        });
-      }
-    });
-  });
+  promises.readdir(sourceFolderPath, { withFileTypes: true }).then((files) =>
+    files.forEach(
+      (file) => {
+        if (file.isFile()) {
+          const sourceFilePath = path.join(sourceFolderPath, file.name);
+          const distinationFilePath = path.join(
+            distanationFolderPath,
+            file.name,
+          );
+          fs.copyFile(sourceFilePath, distinationFilePath, (err) => {
+            if (err) console.error(err);
+          });
+        }
+      },
+      (err) => {
+        console.error(err);
+      },
+    ),
+  );
 })();
