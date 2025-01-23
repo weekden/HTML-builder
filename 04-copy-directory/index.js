8f0034469f32ab4340ac669137226163ccc4ca74
DenisNedelko <dnsndlk@gmail.com>
2025-01-23 17:55:34 +0300

    fix: fixed copyFile function delete dist folder befor created 2025-01-23 17:55:34

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,27 +6,34 @@ const sourceFolderPath = path.join(__dirname, './files');
 const distanationFolderPath = path.join(__dirname, `./${copyDirectoryName}`);
 
 (function copyFile() {
-  fs.mkdir(distanationFolderPath, { recursive: true }, (err) => {
-    if (err) console.error(err);
-    console.log(`Directory <${copyDirectoryName}> was created`);
-  });
-  promises.readdir(sourceFolderPath, { withFileTypes: true }).then((files) =>
-    files.forEach(
-      (file) => {
-        if (file.isFile()) {
-          const sourceFilePath = path.join(sourceFolderPath, file.name);
-          const distinationFilePath = path.join(
-            distanationFolderPath,
-            file.name,
-          );
-          fs.copyFile(sourceFilePath, distinationFilePath, (err) => {
-            if (err) console.error(err);
-          });
+  promises.rm(distanationFolderPath, { recursive: true, force: true }).then(
+    () => {
+      fs.mkdir(distanationFolderPath, { recursive: true }, (err) => {
+        if (err) {
+          console.error(err);
+          return;
         }
-      },
-      (err) => {
-        console.error(err);
-      },
-    ),
+        console.log(`Directory <${copyDirectoryName}> was created`);
+        promises
+          .readdir(sourceFolderPath, { withFileTypes: true })
+          .then((files) =>
+            files.forEach((file) => {
+              if (file.isFile()) {
+                const sourceFilePath = path.join(sourceFolderPath, file.name);
+                const distanationFilePath = path.join(
+                  distanationFolderPath,
+                  file.name,
+                );
+                fs.copyFile(sourceFilePath, distanationFilePath, (err) => {
+                  if (err) {
+                    console.error(err);
+                  }
+                });
+              }
+            }),
+          );
+      });
+    },
+    (err) => console.error(err),
   );
 })();
